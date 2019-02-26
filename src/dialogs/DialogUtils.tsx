bf6dc4122736aab3aaafb1f1681b698272457f60
Quân trịnh văn <quanptit410@gmail.com>
2019-02-26 14:36:40 +0700

    first init

--- src/dialogs/DialogUtils.tsx
+++ src/dialogs/DialogUtils.tsx
@@ -1,9 +1,9 @@
 import React, {ReactChild} from 'react'
 import OverflayContainer, {PositionAnchor} from "./OverflayContainer";
 import {PopupMenuComponent, SubMenuSettingObj} from "./PopupMenuComponent";
-import {sendError} from "my-rn-base-utils";
+import {PreferenceUtils, sendError} from "my-rn-base-utils";
 import {BaseDialog} from "..";
-import {AlertButton} from "react-native";
+import {Alert,  AlertButton} from "react-native";
 import CommonDialog from "./CommonDialog";
 
 export class DialogUtils {
@@ -23,6 +23,72 @@ export class DialogUtils {
         DialogUtils.showDialog(<CommonDialog style={isTopScreen ? {justifyContent: "flex-start", paddingTop: 80} : undefined}
                                              title={title} message={message} dismissOnTouchOutside={isCancelable} btnOk={buttonOk} btnCancel={buttonCancel}
                                              renderOtherChild={renderOtherChild}/>)
+    }
+
+
+    /**
+     * buttonOk, buttonCancel có dạng: {text, onPress}
+     * */
+    public static showQuestionDialog(title: string, message: string, buttonOk: AlertButton, buttonCancel: AlertButton = null, cancelableForAndroid = true) {
+        if (buttonOk == undefined)
+            buttonOk = {text: "OK"};
+        else if (buttonOk.text == undefined)
+            buttonOk.text = "OK";
+
+        if (buttonCancel == undefined)
+            buttonCancel = {text: "CANCEL"};
+
+        buttonCancel.style = 'cancel';
+        Alert.alert(title, message,
+            [buttonCancel, buttonOk],
+            {cancelable: cancelableForAndroid}
+        )
+    }
+
+    public static showInfoDialog(title?: string, message?: string, buttonOk?: AlertButton, cancelableForAndroid?: boolean) {
+        if (buttonOk == undefined)
+            buttonOk = {text: "OK"};
+        else if (buttonOk.text == undefined)
+            buttonOk.text = "OK";
+
+        Alert.alert(title, message,
+            [buttonOk],
+            {cancelable: cancelableForAndroid}
+        )
+    }
+
+    public static async showRateDialogIfNeed(review_title: string, review_description: string,
+                                             yes_sure: string, remind_me_late: string, androidID: string, iosId: string): Promise<boolean> {
+        let hasVote = await PreferenceUtils.getBooleanSetting("HAS_VOTE_APP");
+        let numberPlaycount = await PreferenceUtils.getNumberSetting("PLAY_COUNT");
+        let isNumberMatch = false;
+        if (numberPlaycount === 15)
+            isNumberMatch = true;
+        else if (numberPlaycount > 0 && numberPlaycount % 30 === 0)
+            isNumberMatch = true;
+        let canShow = (!hasVote) && isNumberMatch;
+        numberPlaycount++;
+        await PreferenceUtils.saveSeting("PLAY_COUNT", numberPlaycount);
+
+        if (canShow) {
+            console.log("show dialog rate");
+            this.showQuestionDialog(review_title, review_description, {
+                text: yes_sure, onPress: () => {
+                    this.showRatePage(androidID, iosId);
+                }
+            }, {text: remind_me_late});
+            return true;
+        }
+        return false;
+    }
+
+    public static showRatePage(androidID: string, iosId: string) {
+        if (isIOS()) {
+            Linking.openURL("https://itunes.apple.com/app/id" + iosId + "#").catch(err => console.error('An error occurred', err))
+        } else {
+            RNAppUtils.ANDROID_openAppFromMarket(androidID)
+        }
+        PreferenceUtils.saveBooleanSettingCallback("HAS_VOTE_APP", true)
     }
 
     public static hideDialog() {
