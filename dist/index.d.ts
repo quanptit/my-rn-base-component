6169cf085dcf344a628db565c40a00ed8278ee5e
Quân trịnh văn <quanptit410@gmail.com>
2019-03-08 20:29:59 +0700

    first init

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -9,7 +9,7 @@ export { Button, ButtonModel } from "./Button";
 export { ComponentUpdateOnlyState } from "./base/ComponentUpdateOnlyState";
 export { PureComponentSkipFunction } from "./base/PureComponentSkipFunction";
 export { ComponentNoUpdate } from "./base/ComponentNoUpdate";
-export { Col } from "./Col";
+export { Col, ContainerProps } from "./Col";
 export { Row } from "./Row";
 export { Touchable } from "./Touchable";
 export { BaseDialog } from "./base/BaseDialog";
